83e5ea3159b0fcc66479abdded71859b78f1d45a
Danilo Silva <danilo.dos.s.silva@gmail.com>
2021-06-29 15:09:22 -0300

    Inserção das variáveis 'foto' e 'tipo' com seus Getters & Setters

--- src/main/java/org/generation/blogPessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -6,7 +6,9 @@ public class UserLogin {
 	private String usuario;
 	private String senha;
 	private String token;
-	
+	private String foto;
+	private String tipo;
+
 	public String getNome() {
 		return nome;
 	}
@@ -31,4 +33,21 @@ public class UserLogin {
 	public void setToken(String token) {
 		this.token = token;
 	}	
+
+	public String getFoto() {
+		return this.foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
 }
